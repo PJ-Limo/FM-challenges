06b9440183b516ffc8e66637df89dc0f968b9818
PJ-Limo <pjslense@gmail.com>
2025-01-19 15:18:50 +0300

    installed dependencies and setup

---
+++ mortgage-calculator-challenge/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html/**/*.{js,ts,jsx,tsx,mdx}",
+    "./styles.css/**/*.{js,ts,jsx,tsx,mdx}",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
